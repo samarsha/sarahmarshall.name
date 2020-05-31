982d2ac5cdadc241a6ea1deddc8a8dd0f7f1cb63
Sarah Marshall <sarah@sarahmarshall.name>
2020-05-30 18:16:40 -0700

    Show last 5 posts on home page

--- Main.hs
+++ Main.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-- | The static site generator for www.sarahmarshall.name.
 module Main (main) where
 
 import GHC.IO.Encoding
 import Hakyll
 import Text.Pandoc.Options
 
--- | The site generator.
+-- | Runs the site generator.
 main :: IO ()
 main = setLocaleEncoding utf8 >> hakyll rules
 
@@ -16,17 +17,26 @@ rules = do
     match "assets/**" $ do
         route idRoute
         compile copyFileCompiler
-    match "templates/*" $ compile templateBodyCompiler
-    match "index.md" $ markdown "templates/home.html"
-    match "blog/*.md" $ markdown "templates/post.html"
 
--- | Compiles a Markdown file to an HTML page using the template.
-markdown :: Identifier -> Rules ()
-markdown template = do
+    match "templates/*" $ compile templateBodyCompiler
+
+    match "index.md" $ markdown "templates/home.html" $ do
+        posts <- take 5 <$> loadAll "blog/*" >>= recentFirst
+        return $ defaultContext <>
+            listField "posts" defaultContext (return posts)
+
+    match "blog/*.md" $ markdown "templates/post.html" $ return defaultContext
+
+-- | Compiles a Markdown file to an HTML page using the template and context.
+markdown :: Identifier -> Compiler (Context String) -> Rules ()
+markdown template context = do
     route $ setExtension "html"
-    compile $ pandocCompilerWith
-        defaultHakyllReaderOptions
-        defaultHakyllWriterOptions { writerHTMLMathMethod = KaTeX defaultKaTeXURL }
-        >>= loadAndApplyTemplate template defaultContext
-        >>= loadAndApplyTemplate "templates/page.html" defaultContext
-        >>= relativizeUrls
+    compile $ do
+        ctx <- context
+        pandocCompilerWith readerOptions writerOptions
+            >>= loadAndApplyTemplate template ctx
+            >>= loadAndApplyTemplate "templates/page.html" defaultContext
+            >>= relativizeUrls
+  where
+    readerOptions = defaultHakyllReaderOptions
+    writerOptions = defaultHakyllWriterOptions { writerHTMLMathMethod = KaTeX defaultKaTeXURL }
